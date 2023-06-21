cd457f02b7ebc0dc6f900c8cc5f8dd59a01421fb
universe234 <kehindeodulana2@gmail.com>
2023-06-20 20:14:29 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - all possible combinations
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ones = '0';
+	int tems = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; hundreds <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundreds > tens)))
+				{
+					putchar(hunreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					{
+						putchar(',');
+						putcher(' ');
+					}
+				}
+			}
+		}
+	}
+}
