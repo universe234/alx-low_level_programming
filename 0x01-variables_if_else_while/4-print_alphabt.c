8eff2ff8a8dc5065c8ec2a36c7f4186bfb6f8724
universe234 <kehindeodulana2@gmail.com>
2023-06-20 18:28:20 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point where the project begins
- * Return Always 0 (success)
+ * main - Prog Entry point
+ * Return: Always 0 (success)
  */
 
 int main(void)
